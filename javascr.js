7c48347e31c3142168515b23ed4c1e78749cf578
thanhthu1204 <166897393+thanhthu1204@users.noreply.github.com>
2024-06-13 21:14:14 +0700

    Add files via upload

---
+++ javascr.js
@@ -0,0 +1,13 @@
+function Haydoanho() {
+    let randomNumber = Math.floor(Math.random() * 2);
+    let x;
+    while (x != randomNumber) {
+        x = prompt("Đoán số (0 hoặc 1):");
+        x = parseInt(x);
+        if (x === randomNumber) {
+            alert("Congratulation! Bạn đã đoán đúng số.");
+        } else {
+            alert("Nah. Hãy thử lại.");
+        }
+    }
+}
